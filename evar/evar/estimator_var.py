ef45692076d8949805669c56a50bd662f1153776
motuzov <motuzov@gmail.com>
2025-03-02 19:54:03 +0000

    CalibratorFactory is set in the EstimatorVar's  constructor to use the same calibration for each estimator

--- evar/evar/estimator_var.py
+++ evar/evar/estimator_var.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
-from typing import Protocol, Any
+from numpy.typing import NDArray, DTypeLike, ArrayLike
+from typing import Protocol, Any, Iterator
+from evar.data import COLUMN_NAMES
 
 
 from sklearn.calibration import calibration_curve
-from sklearn.model_selection import GroupKFold
 from sklearn.base import clone
+from collections.abc import Callable
 
 
 class Predictor(Protocol):
@@ -17,117 +19,137 @@ class Estimator(Protocol):
     def predict_proba(self, X: np.ndarray): ...
 
 
-class CalibrationMap:
+INTERVALS_INFO_FOMAT = "{0:.2f}"
+
+
+def intervals_info(bins: ArrayLike) -> pd.Series:
+    f = INTERVALS_INFO_FOMAT.format
+    s_bins = pd.Series(bins)
+    return (
+        "["
+        + s_bins.shift(1).fillna(0).apply(f).str.cat(s_bins.apply(f), sep=", ")
+        + "]"
+    )
+
+
+class Calibrator:
+    def __init__(
+        self, prob_bins: ArrayLike, prob_estim: ArrayLike, mean_score: ArrayLike
+    ):
+        prob_bins = prob_bins[1:]
+        i_info = intervals_info(prob_bins)
+        binid_prob_estim_map = pd.DataFrame(
+            {
+                COLUMN_NAMES.p_estim: prob_estim,
+                COLUMN_NAMES.mean_score: mean_score,
+                COLUMN_NAMES.bin_interval: i_info,
+            }
+        )
+
+        self._prob_bins: ArrayLike = prob_bins
+        self._binid_prob_estim_map: pd.DataFrame = binid_prob_estim_map
+        self._debug_info = {}
+
+    def __call__(self, y_score: ArrayLike) -> pd.DataFrame:
+        y_score_binids = np.searchsorted(self._prob_bins, y_score)
+        df_y_score_binids = pd.DataFrame(
+            {
+                COLUMN_NAMES.binid: y_score_binids,
+                COLUMN_NAMES.y_score: y_score,
+            }
+        )
+        y_calib_results = pd.merge(
+            df_y_score_binids,
+            self._binid_prob_estim_map,
+            left_on=COLUMN_NAMES.binid,
+            right_index=True,
+        )
+        return y_calib_results
+
+    def save_debug_info(self, k: str, v: Any):
+        self._debug_infop[k] = v
+
+
+class CalibratorFactory:
     def __init__(self, y_true_calib: np.array, X_calib: np.ndarray = None):
         self.y_true: np.array = y_true_calib
         self.y_true.flags.writeable = False
         self.X: np.ndarray = X_calib
         if self.X is not None:
             self.X.flags.writeable = False
+        self._bebug = True
         # enable_metadata_routing=True
 
-    def _make_map_wiht_y_score(
-        self, n_bins: int, y_score: np.array
-    ) -> tuple[Any, pd.DataFrame]:
+    def _make_map_wiht_y_score(self, n_bins: int, y_score: np.array) -> Calibrator:
         self.n_bins = n_bins
-        prob_estim, self._prob_pred = calibration_curve(
+        prob_estim, mean_score = calibration_curve(
             self.y_true, y_score, n_bins=n_bins, strategy="quantile"
         )
-        # we need to store the bins to calibrate new predictions with it
+        # We need to store the bins to calibrate predictions with it.
         quantiles = np.linspace(0, 1, n_bins + 1)
         bins = np.percentile(y_score, quantiles * 100)
-        self.first_bin_bound = bins[0]
-        bins[0] = 0
-        range_str = [
-            f"({round(b[0], 4)}; {round(b[1], 4)})" for b in zip(bins[:-1], bins[1:])
-        ]
-        self.df_binid_prob_estim_map = pd.DataFrame(
-            {
-                "p_estim": prob_estim,
-                "calib_range": range_str,
-            }
+        calibrator = Calibrator(
+            prob_bins=bins, prob_estim=prob_estim, mean_score=mean_score
         )
-        self.bins = bins[1:]
+        return calibrator
 
-    def make(self, clf: Predictor, n_bins: int) -> tuple[Any, pd.DataFrame]:
+    def create(self, clf: Predictor, n_bins: int) -> Calibrator:
         y_score = clf.predict_proba(self.X)
         y_score = y_score[:, 1]
-        self._make_map_wiht_y_score(n_bins=n_bins, y_score=y_score)
+        return self._make_map_wiht_y_score(n_bins=n_bins, y_score=y_score)
 
 
 class CalibratedProbPredictor(Predictor):
-    def __init__(self, clf: Predictor, prob_bins: int, calibration_map: CalibrationMap):
-        # binds the calibrator and and the predictor
-        if clf:
-            calibration_map.make(clf, prob_bins)
-        self._bins = calibration_map.bins
-        self._df_binid_prob_estim_map = calibration_map.df_binid_prob_estim_map
+    def __init__(self, clf: Predictor, calibrator: Calibrator):
+        self._calibrate = calibrator
         self._clf = clf
-
-    def calibrate(self, y_score: np.array):
-        y_score_binids = np.searchsorted(self._bins, y_score)
-        df_y_score_binids = pd.DataFrame(
-            {
-                "binid": y_score_binids,
-                "y_score": y_score,
-            }
-        )
-        y_calib_results = pd.merge(
-            df_y_score_binids,
-            self._df_binid_prob_estim_map,
-            left_on="binid",
-            right_index=True,
-        )
-        return y_calib_results
 
     def predict_proba(self, X):
         y_score = self._clf.predict_proba(X)
         y_score = y_score[:, 1].ravel()
-        y_calib_info = self.calibrate(y_score=y_score)
+        y_calib_info = self._calibrate(y_score=y_score)
         return y_calib_info
 
 
 class EstimatorVar:
-    # the estimator states??
     def __init__(
-        self, X_train: np.ndarray, y_train: np.array, calibration_map: CalibrationMap
+        self,
+        X_train: np.ndarray,
+        y_train: np.array,
+        estimator: Estimator,
+        calibration_factory: CalibratorFactory,
     ):
-        self.calibration_map = calibration_map
+        self.calibration_factory: CalibratorFactory = calibration_factory
         self.prob_predictors: list[CalibratedProbPredictor] = []
         self.X_train = X_train
         self.y_train = y_train
+        self.estimator = estimator
 
-    def fit_clfs(
+    def fit_prob_predictors(
         self,
-        estimator: Estimator,
-        splits,
-        prob_bins,
+        splitter: Callable[[DTypeLike], Iterator[tuple[np.ndarray, np.ndarray]]],
+        prob_bins: int,
+        n_splits: int,
     ):
-        # fit predictors acorodng to groups (D_i - i group data):
+        # Fit f_hat calibrated predictions according to groups using the same calibration:
         # f_hat(D_i), D_i is i's group data
         self.prob_predictors = []
-        for train_index, test_index in splits:
-            clf = clone(estimator)
+        for train_index, test_index in splitter(self.X_train, n_splits):
+            clf = clone(self.estimator)
             clf.fit(self.X_train[train_index], self.y_train[train_index])
-            prob_pred = CalibratedProbPredictor(
-                clf=clf,
-                prob_bins=prob_bins,
-                calibration_map=self.calibration_map,
-            )
+            calibrator = self.calibration_factory.create(clf, prob_bins)
+            prob_pred = CalibratedProbPredictor(clf=clf, calibrator=calibrator)
             self.prob_predictors.append(prob_pred)
-
-    def _point_wise_var(self, df_var_data):
-        self.point_wise_var = df_var_data.groupby(level=0)["p_estim"].var()
 
     def _bin_wise_var(self, df_var_data: pd.DataFrame):
         fixed_pred_id = 0
         fixed_binid = 0
         df_points_in_fixed_bin = df_var_data[
-            (df_var_data["binid"] == fixed_binid)
-            & (df_var_data["pred_id"] == fixed_pred_id)
+            (df_var_data[COLUMN_NAMES.binid] == fixed_binid)
+            & (df_var_data[COLUMN_NAMES.p_predid] == fixed_pred_id)
         ]
         df_points_of_fixed_bin = pd.merge(
-            left=df_var_data[df_var_data["pred_id"] != fixed_pred_id],
+            left=df_var_data[df_var_data[COLUMN_NAMES.p_predid] != fixed_pred_id],
             right=df_points_in_fixed_bin,
             how="inner",
             left_index=True,
@@ -137,63 +159,26 @@ class EstimatorVar:
         diff_bin_points = df_points_of_fixed_bin[
             df_points_of_fixed_bin[f"binid_{fixed_pred_id}"] != fixed_pred_id
         ]
-        (diff_bin_points["p_estim_0"] - diff_bin_points["p_estim"]).var()
+        (diff_bin_points["p_estim_0"] - diff_bin_points[COLUMN_NAMES.p_predid]).var()
 
-    def _var_data_prep(self, X_test: np.ndarray) -> pd.DataFrame:
+    def _point_wise_var(self, df_var_data: pd.DataFrame):
+        self.point_wise_var = df_var_data.groupby(level=0)[COLUMN_NAMES.p_predid].var()
+
+    def _var_data_prep(self, X_test: np.ndarray, y_test: np.ndarray) -> pd.DataFrame:
         prob_pred_dfs = []
         for i, prob_pred in enumerate(self.prob_predictors):
             df_prob_pred = prob_pred.predict_proba(X_test)
-            df_prob_pred["pred_id"] = i
-            df_prob_pred["point_id"] = df_prob_pred.index
+            df_prob_pred[COLUMN_NAMES.p_predid] = i
             prob_pred_dfs.append(df_prob_pred)
-        return pd.concat(prob_pred_dfs)
 
-    def estimate(self, X_test: np.ndarray):
-        self.df_var_data = self._var_data_prep(X_test)
-        self._pred_bin_var(self.df_var_data)
+        self._var_data = pd.concat(prob_pred_dfs)
+        self._var_data[COLUMN_NAMES.y_test] = pd.DataFrame(y_test)
+        return self._var_data
 
+    @property
+    def var_data(self):
+        return self._var_data
 
-def test_calibrator():
-    y_true = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1])
-    y_score = np.array([0.1, 0.2, 0.3, 0.4, 0.65, 0.7, 0.8, 0.9, 1.0])
-    calibration_map = CalibrationMap(y_true_calib=y_true)
-    print("make calibration")
-    calibration_map._make_map_wiht_y_pred(n_bins=3, y_score=y_score)
-    print(f"round binds {np.round(calibration_map.bins, decimals=3)}")
-    round_binds_true = [0.367, 0.733, 1.0]
-    print((round_binds_true == np.round(calibration_map.bins, decimals=3)).all())
-    new_y_pred = np.array([0.2, 0.05, 0.5, 0.3, 0.8])
-    # (bin=interavl):
-    #  0=-(0, 0.367), 1=-(0.367, 0.733), 2=(0.733, INF)
-    #  0.2 -> 0, 0.05 -> 0
-    print(calibration_map.df_binid_prob_estim_map)
-    # res = calibrator.calibrate(y_pred_to_calibrate=new_y_pred)
-    # print(res)
-
-
-def test_groups():
-    X = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
-    y = np.array([1, 2, 3, 4, 5, 6])
-    groups = np.array([0, 0, 2, 2, 3, 3])
-    group_kfold = GroupKFold(n_splits=2)
-    group_kfold.get_n_splits(X, y, groups)
-    for train_index, test_index in group_kfold.split(X, y, groups):
-        print("tarin\\tesst splitting")
-        print("X train")
-        print(X[train_index])
-        print("X test")
-        print(X[test_index])
-        print("\n")
-
-
-if __name__ == "__main__":
-    test_calibrator()
-    # test_groups()
-
-
-# workwlow cross_val_predict -> y_pred
-# model_selection > use estimator = estimator.fit(data, targets) -> to calc -> estmator-var
-#  predict_proba
-# optuna layer: objective estmator-var to contrl bias var trade-off
-# xgb.cb.cv.predict() return the test fold predictions from each CV model.
-# y_predicted = estimator(alpha=10).fit(X_train, y_train).predict(X_test)
+    def estimate(self):
+        self.df_var_data = self._var_data_prep(self.X_test)
+        self._point_wise_var(self.df_var_data)
--- evar/tests/test_evar.py
+++ evar/tests/test_evar.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
-from evar.estimator_var import CalibratedProbPredictor, CalibrationMap
+from evar.estimator_var import (
+    CalibratorFactory,
+    Calibrator,
+    intervals_info,
+)
 
 
 import pytest
@@ -8,20 +12,42 @@ from pandas import testing as tm
 
 
 @pytest.fixture
-def calibration_map():
+def calibrator_factory() -> CalibratorFactory:
     y_true = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1])
-    c_map = CalibrationMap(y_true_calib=y_true)
-    y_score = np.array([0.1, 0.2, 0.3, 0.4, 0.65, 0.7, 0.8, 0.9, 1.0])
-    c_map._make_map_wiht_y_score(n_bins=3, y_score=y_score)
-    return c_map
+    return CalibratorFactory(y_true_calib=y_true)
 
 
-def test_prob_predictor_calibrate(calibration_map):
-    prob_pred = CalibratedProbPredictor(
-        clf=None, prob_bins=3, calibration_map=calibration_map
+@pytest.fixture
+def y_score() -> np.array:
+    return np.array([0.1, 0.2, 0.3, 0.4, 0.65, 0.7, 0.8, 0.9, 1.0])
+
+
+def test_intervals_info():
+    bins = np.array([0.1, 0.36666667, 0.73333333, 1.0])
+    bin_intervals = intervals_info(bins)
+    tm.assert_series_equal(
+        left=bin_intervals,
+        right=pd.Series(
+            [
+                "[0.00, 0.10]",
+                "[0.10, 0.37]",
+                "[0.37, 0.73]",
+                "[0.73, 1.00]",
+            ]
+        ),
+        check_names=False,
     )
+
+
+@pytest.fixture
+def calibrator(calibrator_factory, y_score) -> CalibratorFactory:
+    calibrator_ = calibrator_factory._make_map_wiht_y_score(n_bins=3, y_score=y_score)
+    return calibrator_
+
+
+def test_calibrate(calibrator: Calibrator):
     y_score = np.array([0.2, 0.05, 0.5, 0.3, 0.8])
-    y_calib_results = prob_pred.calibrate(y_score=y_score)
+    y_calib_results = calibrator(y_score=y_score)
     tm.assert_series_equal(
         y_calib_results["p_estim"].round(decimals=2),
         pd.Series([0.0, 0.0, 0.67, 0.0, 1.0]),
