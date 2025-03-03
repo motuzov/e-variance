c8885d9921dc210c6b40d90118f48cf846f75d4d
motuzov <motuzov@gmail.com>
2025-03-03 20:36:19 +0000

    fix the uniqueness of a bin id

--- evar/evar/viz.py
+++ evar/evar/viz.py
@@ -5,12 +5,13 @@ from evar.data import COLUMN_NAMES
 
 
 def plot_bin_pred_hist(var_data: pd.DataFrame):
-    s = var_data.groupby(level=0)[COLUMN_NAMES.binid].unique().astype(str)
-    s.groupby(s).count()
+    s = var_data.groupby(level=0)[COLUMN_NAMES.binid].agg(set).astype("str")
     sns.barplot(s.groupby(s).count(), color="lightgreen", edgecolor="red")
-    plt.xlabel("Bin sets")
+    plt.xlabel("Porb bin sets")
     plt.ylabel("Count")
     plt.title(
-        "A histogram of the number of unique bins the predictors put a single data point in."
+        """
+        A histogram of the number of unique bins the predictors put a single data point in
+        """
     )
     plt.show()
